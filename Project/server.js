99a37fd0ee8b88b7deeacb060fd3c34fa3ed8483
Pasindu Rukshan <89061364+pasindurukshan@users.noreply.github.com>
2022-01-20 16:37:32 +0530

    Update server.js

--- Project/server.js
+++ Project/server.js
@@ -87,7 +87,19 @@ app.use(orderRoutes);
 //route qualitycheck midleware
 app.use(postRoutes);
 
-const PORT = 8000;
+
+//server static assests if in production - for Heroku
+if (process.env.NODE_ENV == 'production') { 
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => { 
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+const PORT = process.env.PORT || 8000;
 const DB_URL =
   "mongodb+srv://itpcasanova:cas123@cluster0.jav72.mongodb.net/CasanovaDB?retryWrites=true&w=majority";
 
